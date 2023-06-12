a72d3a8f21655d459cf01d1329aa4ae9cf9dd237
simonbeirouti <hello@simonbeirouti.com>
2023-06-12 17:30:45 +1000

    rmv emailSub component

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,6 @@ import Hero from "@/components/homePage/Hero";
 import BlogList from "@/components/homePage/BlogList";
 import Features from "@/components/homePage/Features";
 import Benefits from "@/components/ui/Benefits";
-import EmailSub from "@/components/ui/EmailSub";
 
 export default function HomePage() {
   return (
@@ -11,7 +10,6 @@ export default function HomePage() {
       <Features />
       <Benefits />
       <BlogList />
-      <EmailSub />
     </>
   );
 }
