f5c397cb3c5453ecfa9bc2f76eb3d9606b3ca8f4
simonbeirouti <hello@simonbeirouti.com>
2023-06-13 13:24:01 +1000

    creation of team and value components

---
+++ src/components/aboutPage/Team.jsx
@@ -0,0 +1,55 @@
+import Image from "next/image";
+
+const team = [
+  {
+    id: 1,
+    name: "Michael Foster",
+    role: "Co-Founder / CTO",
+    imageUrl:
+      "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
+  },
+  {
+    id: 2,
+    name: "Michael Foster",
+    role: "Co-Founder / CTO",
+    imageUrl:
+      "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
+  },
+];
+
+export default function Team() {
+  return (
+    <div className="mx-auto mt-16 max-w-7xl px-6 sm:mt-24 lg:px-8">
+      <div className="mx-auto max-w-2xl lg:mx-0">
+        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+          Our team
+        </h2>
+        <p className="mt-6 text-lg leading-8 text-gray-300">
+          Sit facilis neque ab nulla vel. Cum eos in laudantium. Temporibus eos
+          totam in dolorum. Nemo vel facere repellendus ut eos dolores
+          similique.
+        </p>
+      </div>
+      <ul
+        role="list"
+        className="mx-auto mt-20 grid max-w-2xl grid-cols-2 gap-x-8 gap-y-16 text-center sm:grid-cols-3 md:grid-cols-4 lg:mx-0 lg:max-w-none lg:grid-cols-5 xl:grid-cols-6"
+      >
+        {team.map((person) => (
+          <li key={person.name}>
+            <Image
+              className="mx-auto h-24 w-24 rounded-full"
+              src={person.imageUrl}
+              alt=""
+              width={200}
+              height={200}
+            />
+            <h3 className="mt-6 text-base font-semibold leading-7 tracking-tight text-gray-900">
+              {person.name}
+            </h3>
+            <p className="text-sm leading-6 text-gray-300">{person.role}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
