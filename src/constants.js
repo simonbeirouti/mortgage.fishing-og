c7002578c9b0264cfb62cc04a8306d0b71e8a5f9
simonbeirouti <hello@simonbeirouti.com>
2023-06-13 12:40:25 +1000

    create constants files

---
+++ src/constants.js
@@ -0,0 +1,16 @@
+export const categories = [
+  { name: "Marketing", href: "/categories/marketing", image: "/fish-logo.png" },
+  { name: "Mortgage", href: "/categories/mortgage", image: "/fish-logo.png" },
+  { name: "Marketing", href: "/categories/marketing", image: "/fish-logo.png" },
+];
+
+export const navigation = [
+  { name: "About us", href: "/about-us" },
+  { name: "Contact", href: "/contact" },
+];
+
+export const company = [
+  { name: "Careers", href: "/careers" },
+  { name: "Terms & conditions", href: "/terms-and-conditions" },
+  { name: "Privacy policy", href: "/privacy-policy" },
+];
