bc89bf833df66486d4f43447fb5712358f58bc13
simonbeirouti <hello@simonbeirouti.com>
2023-06-13 11:31:47 +1000

    add date to introduction

--- src/components/blog/Introduction.jsx
+++ src/components/blog/Introduction.jsx
@@ -1,4 +1,7 @@
-export default function Introduction({ short, title, tldr }) {
+export default function Introduction({ short, date, title, tldr }) {
+  const inputDate = date;
+  const dateObject = new Date(inputDate);
+  const formattedDate = dateObject.toISOString().slice(0, 10);
   return (
     <>
       <p className="text-base font-semibold leading-7 text-indigo-600">
@@ -7,6 +10,9 @@ export default function Introduction({ short, title, tldr }) {
       <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
         {title}
       </h1>
+      <time dateTime={formattedDate} className="mt-8 text-sm text-gray-700">
+        {date}
+      </time>
       <p className="mt-6 text-xl leading-8 text-gray-700">{tldr}</p>
     </>
   );
