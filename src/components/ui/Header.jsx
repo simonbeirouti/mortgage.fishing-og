090da5304043afa6ea47664622288a06cea16810
simonbeirouti <hello@simonbeirouti.com>
2023-06-13 14:07:48 +1000

    add onClick to close

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -178,6 +178,7 @@ export default function Header() {
                             as={Link}
                             href={item.href}
                             className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                            onClick={() => setMobileMenuOpen(false)}
                           >
                             {item.name}
                           </Disclosure.Button>
@@ -206,6 +207,7 @@ export default function Header() {
                             as={Link}
                             href={item.href}
                             className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                            onClick={() => setMobileMenuOpen(false)}
                           >
                             {item.name}
                           </Disclosure.Button>
