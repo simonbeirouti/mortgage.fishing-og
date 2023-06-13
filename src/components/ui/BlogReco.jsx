cf7fa99624e0536abc018ae1b3128ab513a3e311
simonbeirouti <hello@simonbeirouti.com>
2023-06-13 23:14:32 +1000

    rmv comment

--- src/components/ui/BlogReco.jsx
+++ src/components/ui/BlogReco.jsx
@@ -58,14 +58,6 @@ export default function BlogReco() {
   return (
     <div className="bg-white pb-24 sm:pb-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        {/* <div className="mx-auto max-w-2xl lg:mx-0">
-          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-            From the blog
-          </h2>
-          <p className="mt-2 text-lg leading-8 text-gray-600">
-            Other posts to learn more
-          </p>
-        </div> */}
         <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-8 border-t border-timber pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
           {posts.map((post) => (
             <article
