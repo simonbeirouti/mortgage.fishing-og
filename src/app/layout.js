7c982beba84b5f707964705e2be2918c8f194ab0
simonbeirouti <hello@simonbeirouti.com>
2023-06-13 23:24:01 +1000

    move metadata layout to page

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,12 +2,6 @@ import Header from "@/components/ui/Header";
 import Footer from "@/components/ui/Footer";
 import "./globals.css";
 
-export const metadata = {
-  title: "Mortgage Fishing | Home",
-  description:
-    "Home page for mortgage fishing so you can learn more and find the best mortgage deals",
-};
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,12 @@ import BlogList from "@/components/homePage/BlogList";
 import Features from "@/components/homePage/Features";
 import Benefits from "@/components/ui/Benefits";
 
+export const metadata = {
+  title: "Mortgage Fishing | Home",
+  description:
+    "Unlock financial freedom with our expert insights and practical solutions. Discover valuable resources to navigate personal and business finance.",
+};
+
 export default function HomePage() {
   return (
     <>
