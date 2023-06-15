aa3fbfed0ebbfb9f42e0371d231f89e6687c25c6
simonbeirouti <hello@simonbeirouti.com>
2023-06-15 22:31:26 +1000

    add blogPosts function and link to BlogList

--- src/components/homePage/BlogList.jsx
+++ src/components/homePage/BlogList.jsx
@@ -1,56 +1,15 @@
-import Image from "next/image";
+// import Image from "next/image";
 import Link from "next/link";
-
-const featuredPost = {
-  id: 1,
-  title: "We are incredibly proud to announce we have secured $75m in Series B",
-  href: "secured-75m-in-series-b",
-  description:
-    "Libero neque aenean tincidunt nec consequat tempor. Viverra odio id velit adipiscing id. Nisi vestibulum orci eget bibendum dictum. Velit viverra posuere vulputate volutpat nunc. Nunc netus sit faucibus.",
-  date: "Mar 16, 2020",
-  datetime: "2020-03-16",
-  author: {
-    name: "Michael Foster",
-    href: "#",
-    imageUrl:
-      "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-  },
-};
-
-const posts = [
-  {
-    id: 2,
-    title: "Boost your conversion rate",
-    href: "boost-your-conversion-rate",
-    description:
-      "Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel iusto corrupti dicta laboris incididunt.",
-    date: "Mar 10, 2020",
-    datetime: "2020-03-16",
-    author: {
-      name: "Lindsay Walton",
-      href: "#",
-      imageUrl:
-        "https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-    },
-  },
-  {
-    id: 3,
-    title: "Boost your conversion rate",
-    href: "boost-your-conversion-rate-again",
-    description:
-      "Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel iusto corrupti dicta laboris incididunt.",
-    date: "Mar 10, 2020",
-    datetime: "2020-03-16",
-    author: {
-      name: "Lindsay Walton",
-      href: "#",
-      imageUrl:
-        "https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-    },
-  },
-];
+import { getSortedPostsData } from "../../../lib/posts";
 
 export default function BlogList() {
+  const posts = getSortedPostsData().slice(0, 3);
+  const featuredPost = posts[0];
+  const standardPosts = posts.slice(1, 3);
+  // console.log(posts);
+  // console.log(featuredPost);
+  // console.log(standardPosts);
+
   return (
     <div className="bg-white pb-12 sm:pb-16">
       <div className="mx-auto max-w-2xl text-center mb-20">
@@ -62,86 +21,11 @@ export default function BlogList() {
         </p>
       </div>
       <div className="mx-auto grid max-w-7xl grid-cols-1 gap-x-8 gap-y-6 px-6 sm:gap-y-16 lg:grid-cols-2 lg:px-8">
-        <article className="mx-auto w-full max-w-2xl lg:mx-0 lg:max-w-lg">
-          <time
-            dateTime={featuredPost.datetime}
-            className="block text-sm leading-6 text-gray"
-          >
-            {featuredPost.date}
-          </time>
-          <h2
-            id="featured-post"
-            className="mt-4 text-3xl font-bold tracking-tight text-black sm:text-4xl"
-          >
-            {featuredPost.title}
-          </h2>
-          <p className="mt-4 text-lg leading-8 text-gray">
-            {featuredPost.description}
-          </p>
-          <div className="mt-4 flex flex-col justify-between gap-6 sm:mt-8 sm:flex-row-reverse sm:gap-8 lg:mt-4 lg:flex-col">
-            <div className="flex">
-              <a
-                href={`blog/${featuredPost.href}`}
-                className="text-sm font-semibold leading-6 text-sage"
-                aria-describedby="featured-post"
-              >
-                Continue reading <span aria-hidden="true">&rarr;</span>
-              </a>
-            </div>
-            <div className="flex lg:border-t lg:border-timber lg:pt-8">
-              <a
-                href={featuredPost.author.href}
-                className="flex gap-x-2.5 text-sm font-semibold leading-6 text-gray"
-              >
-                <Image
-                  src={featuredPost.author.imageUrl}
-                  alt=""
-                  className="h-6 w-6 flex-none rounded-full"
-                  width={100}
-                  height={100}
-                />
-                {featuredPost.author.name}
-              </a>
-            </div>
-          </div>
-        </article>
+        <FeaturedPost post={featuredPost} />
         <div className="mx-auto w-full max-w-2xl border-t border-timber pt-12 sm:pt-16 lg:mx-0 lg:max-w-none lg:border-t-0 lg:pt-0">
           <div className="-my-9">
-            {posts.map((post) => (
-              <article key={post.id} className="py-9">
-                <div className="group relative max-w-xl">
-                  <time
-                    dateTime={post.datetime}
-                    className="block text-sm leading-6 text-black"
-                  >
-                    {post.date}
-                  </time>
-                  <h2 className="mt-2 text-lg font-semibold text-black group-hover:text-gray">
-                    <a href={`/blog/${post.href}`}>
-                      <span className="absolute inset-0" />
-                      {post.title}
-                    </a>
-                  </h2>
-                  <p className="mt-4 text-sm leading-6 text-black">
-                    {post.description}
-                  </p>
-                </div>
-                <div className="mt-4 flex">
-                  <a
-                    href={post.author.href}
-                    className="relative flex gap-x-2.5 text-sm font-semibold leading-6 text-gray"
-                  >
-                    <Image
-                      src={post.author.imageUrl}
-                      alt=""
-                      className="h-6 w-6 flex-none rounded-full"
-                      width={100}
-                      height={100}
-                    />
-                    {post.author.name}
-                  </a>
-                </div>
-              </article>
+            {standardPosts.map((post, index) => (
+              <StandardPost key={index} post={post} />
             ))}
           </div>
         </div>
@@ -152,5 +36,89 @@ export default function BlogList() {
         </div>
       </Link>
     </div>
+  );
+}
+
+function FeaturedPost({ post }) {
+  return (
+    <article
+      key={post.id}
+      className="mx-auto w-full max-w-2xl lg:mx-0 lg:max-w-lg"
+    >
+      <time dateTime={post.date} className="block text-sm leading-6 text-gray">
+        {post.date}
+      </time>
+      <h2
+        id="featured-post"
+        className="mt-4 text-3xl font-bold tracking-tight text-black sm:text-4xl"
+      >
+        {post.title}
+      </h2>
+      <p className="mt-4 text-lg leading-8 text-gray">{post.title}</p>
+      <div className="mt-4 flex flex-col justify-between gap-6 sm:mt-8 sm:flex-row-reverse sm:gap-8 lg:mt-4 lg:flex-col">
+        <div className="flex">
+          <a
+            href={`blog/${post.href}`}
+            className="text-sm font-semibold leading-6 text-sage"
+            aria-describedby="featured-post"
+          >
+            Continue reading <span aria-hidden="true">&rarr;</span>
+          </a>
+        </div>
+        {/* <div className="flex lg:border-t lg:border-timber lg:pt-8">
+          <a
+            href={href}
+            className="flex gap-x-2.5 text-sm font-semibold leading-6 text-gray"
+          >
+            <Image
+              // src={imageUrl}
+              src=""
+              alt=""
+              className="h-6 w-6 flex-none rounded-full"
+              width={100}
+              height={100}
+            />
+            {name}
+          </a>
+        </div> */}
+      </div>
+    </article>
+  );
+}
+
+function StandardPost({ post }) {
+  return (
+    <article key={post.id} className="py-9">
+      <div className="group relative max-w-xl">
+        <time
+          dateTime={post.datetime}
+          className="block text-sm leading-6 text-black"
+        >
+          {post.date}
+        </time>
+        <h2 className="mt-2 text-lg font-semibold text-black group-hover:text-gray">
+          <a href={`/blog/${post.href}`}>
+            <span className="absolute inset-0" />
+            {post.title}
+          </a>
+        </h2>
+        <p className="mt-4 text-sm leading-6 text-black">{post.description}</p>
+      </div>
+      {/* <div className="mt-4 flex">
+        <a
+          href={href}
+          className="relative flex gap-x-2.5 text-sm font-semibold leading-6 text-gray"
+        >
+          <Image
+            src={imageUrl}
+            alt=""
+            className="h-6 w-6 flex-none rounded-full"
+            width={100}
+            height={100}
+          />
+          {name}
+        </a>
+      </div> */}
+    </article>
   );
 }
