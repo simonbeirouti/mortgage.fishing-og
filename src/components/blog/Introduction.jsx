c29508be090b6f77de277c618935dc799b46d3c3
simonbeirouti <hello@simonbeirouti.com>
2023-06-16 22:07:46 +1000

    fix styling to date

--- src/components/blog/Introduction.jsx
+++ src/components/blog/Introduction.jsx
@@ -1,16 +1,10 @@
-export default function Introduction({ short, date, title, tldr }) {
-  const inputDate = date;
-  const dateObject = new Date(inputDate);
-  const formattedDate = dateObject.toISOString().slice(0, 10);
+export default function Introduction({ date, title, tldr }) {
   return (
     <>
-      <p className="text-base font-semibold leading-7 text-indigo-600">
-        {short}
-      </p>
-      <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+      <h1 className="my-6 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
         {title}
       </h1>
-      <time dateTime={formattedDate} className="mt-8 text-sm text-gray-700">
+      <time dateTime={date} className="ext-sm text-gray-700">
         {date}
       </time>
       <p className="mt-6 text-xl leading-8 text-gray-700">{tldr}</p>
