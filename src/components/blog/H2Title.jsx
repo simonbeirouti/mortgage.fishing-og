9aaa065933bf8526284616f5d50d6835c1a3a3c7
simonbeirouti <hello@simonbeirouti.com>
2023-06-16 22:01:45 +1000

    rmv extra components

--- src/components/blog/H2Title.jsx
+++
@@ -1,7 +0,0 @@
-export default function H2Title({ content }) {
-  return (
-    <h2 className="mt-8 text-2xl font-bold tracking-tight text-gray-900">
-      {content}
-    </h2>
-  );
-}
--- src/components/blog/Paragraph.jsx
+++
@@ -1,5 +0,0 @@
-import clsx from "clsx";
-
-export default function Paragraph({ content, isSpace }) {
-  return <p className={clsx({ "mt-6": !!isSpace })}>{content}</p>;
-}
