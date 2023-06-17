10e9fd488fbdd0d9a67af141fbb48ea96b26ce79
simonbeirouti <hello@simonbeirouti.com>
2023-06-17 12:39:39 +1000

    capitalise header title

--- src/components/ui/PageHeader.jsx
+++ src/components/ui/PageHeader.jsx
@@ -3,7 +3,7 @@ export default function PageHeader({ short, page, description }) {
     <div className="bg-white px-6 pb-6 sm:pb-8 lg:px-8">
       <div className="mx-auto max-w-2xl text-center">
         <p className="text-base font-bold leading-7 text-sage-700">{short}</p>
-        <h2 className="mt-2 text-4xl font-bold tracking-tight text-black sm:text-6xl">
+        <h2 className="mt-2 text-4xl capitalize font-bold tracking-tight text-black sm:text-6xl">
           {page}
         </h2>
         <p className="mt-6 text-lg leading-8 text-gray-500">{description}</p>
