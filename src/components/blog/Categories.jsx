760d561b931504f8a33f6f54364f84bf3a895379
simonbeirouti <hello@simonbeirouti.com>
2023-06-17 16:42:41 +1000

    replace a with Link

--- src/components/blog/Categories.jsx
+++ src/components/blog/Categories.jsx
@@ -1,14 +1,16 @@
+import Link from "next/link";
+
 export default function Categories({ categories }) {
   return (
     <div className="mt-10">
       {categories.map((category, index) => (
-        <a
+        <Link
           key={index}
           href={category.href}
           className="relative rounded-full bg-gray-500 px-3 py-1.5 mr-2 font-medium text-white hover:bg-timber hover:text-gray-500"
         >
           {category.name}
-        </a>
+        </Link>
       ))}
     </div>
   );
