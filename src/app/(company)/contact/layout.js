1aa901785b5fb432e27240c03af807ca05bb5027
simonbeirouti <hello@simonbeirouti.com>
2023-07-14 13:16:47 +1000

    add metadata for contact page

--- src/app/(company)/contact/layout.js
+++ src/app/(company)/contact/layout.js
@@ -2,6 +2,24 @@ export const metadata = {
   title: "Mortgage Fishing | Contact",
   description:
     "Reach out to us for personalized financial guidance and support. Connect with our team to discuss your specific needs and queries.",
+  keywords: [
+    "contact mortgage broker",
+    "free advice",
+    "chat about mortgage rates",
+    "chat about interest rates",
+    "mortgage brokers",
+    "mortgage rates informations",
+    "chat for free information",
+    "chat for with broker",
+    "contact broker",
+  ],
+  openGraph: {
+    title: "Mortgage Fishing | Contact",
+    description:
+      "Reach out to us for personalized financial guidance and support. Connect with our team to discuss your specific needs and queries.",
+    url: "https://mortgagefishing.com/contact",
+    type: "website",
+  },
 };
 
 export default function ContactLayout({ children }) {
