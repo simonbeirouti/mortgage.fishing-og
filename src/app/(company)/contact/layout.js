104b70ef62034ea1aa460658772d72f528ec01a8
simonbeirouti <hello@simonbeirouti.com>
2023-07-14 15:54:55 +1000

    add image to openGraph

--- src/app/(company)/contact/layout.js
+++ src/app/(company)/contact/layout.js
@@ -19,6 +19,14 @@ export const metadata = {
       "Reach out to us for personalized financial guidance and support. Connect with our team to discuss your specific needs and queries.",
     url: "https://mortgagefishing.com/contact",
     type: "website",
+    images: [
+      {
+        url: "/assets/presentation.png",
+        width: 500,
+        height: 500,
+        alt: "image of a presenter with a whiteboard",
+      },
+    ],
   },
 };
 
