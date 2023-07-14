d5ca335b070096df03e26d6c741a441887469f98
simonbeirouti <hello@simonbeirouti.com>
2023-07-14 13:16:20 +1000

    add meta data for home page

--- src/app/page.js
+++ src/app/page.js
@@ -7,6 +7,35 @@ export const metadata = {
   title: "Mortgage Fishing | Home",
   description:
     "Unlock financial freedom with our expert insights and practical solutions. Discover valuable resources to navigate personal and business finance.",
+  keywords: [
+    "expert insights",
+    "financial resources",
+    "valuable insights",
+    "expert advice",
+    "personal and business finance",
+    "financial journey",
+    "financial freedom",
+    "financial future",
+    "mortgage broking",
+    "mortgage brokers",
+    "free knowledge",
+    "free information",
+  ],
+  openGraph: {
+    title: "Mortgage Fishing | Home",
+    description:
+      "Unlock financial freedom with our expert insights and practical solutions. Discover valuable resources to navigate personal and business finance.",
+    url: "https://mortgagefishing.com",
+    type: "website",
+    images: [
+      {
+        url: "/assets/house-couple.png",
+        width: 500,
+        height: 500,
+        alt: "image of a couple in front of a house",
+      },
+    ],
+  },
 };
 
 export default function HomePage() {
